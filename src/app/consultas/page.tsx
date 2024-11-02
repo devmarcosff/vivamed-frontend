b76b29757e372011711563650c00b6d81d16a807
unknown <devmarcosff@gmail.com>
2024-11-02 11:12:00 -0300

    fix

--- src/app/consultas/page.tsx
+++ src/app/consultas/page.tsx
@@ -1,18 +1,15 @@
 import { SidebarTrue } from "@/components/Sidebar";
 import ConsultationList from '@/pages/consultation/consultation';
 import { Metadata } from "next";
-import { NextRequest } from "next/server";
-
 export const metadata: Metadata = {
   title: 'Vivamed - Painel de controle'
 }
 
-export default function Consultation(request: NextRequest) {
+export default function Consultation() {
   return (
     <div className="flex">
       <SidebarTrue />
       <div className="w-full md:py-5 py-20 px-5 h-screen">
-        {/* <span>Você está na rota: {pathname}</span> */}
         <ConsultationList />
       </div>
     </div>
